14d902d0b7db0a0508d6f5d878dd75ea1e95bb26
Edward Hesketh <inbox@edwardh.dev>
2025-02-09 16:49:03 +0000

    feat: first commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Reproducible NixOS for a virtual k8s cluster";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@ inputs:
+    let
+      inherit (self) outputs;
+
+      sshkeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvr2FrC9i1bjoVzg+mdytOJ1P0KRtah/HeiMBuKD3DX cardno:23_836_181" # RED
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4ZYYVVw4dsNtzOnBCTXbjuRqOowMOvP3zetYXeE5i+2Strt1K4vAw37nrIwx3JsSghxq1Qrg9ra0aFJbwtaN3119RR0TaHpatc6TJCtwuXwkIGtwHf0/HTt6AH8WOt7RFCNbH3FuoJ1oOqx6LZOqdhUjAlWRDv6XH9aTnsEk8zf+1m30SQrG8Vcclj1CTFMAa+o6BgGdHoextOhGMlTx8ESAlgIXCo+dIVjANE2qbfAg0XL0+BpwlRDJt5OcgzrILXZ1jSIYRW4eg/JBcDW/WqorEummxhB26Y6R0jeswRF3DOQhU2fAhbsCWdairLam42rFGlKfWyTbgjRXl/BNR" # a-h
+      ];
+
+      pkgs = import nixpkgs { };
+    in
+    {
+      nixosConfigurations = {
+        node-1 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs outputs sshkeys pkgs; };
+          modules = [
+            ./shared.nix
+            ./node-1.nix
+          ];
+        };
+      };
+    };
+}
