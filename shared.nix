fc0883982b838b0fba9420fc9339459f9cf7b9b0
Edward Hesketh <inbox@edwardh.dev>
2025-02-09 21:27:27 +0000

    feat: re-enable firewall

--- shared.nix
+++ shared.nix
@@ -26,16 +26,15 @@
     #kubernetes
   ];
 
-  networking.firewall.enable = lib.mkForce false;
   networking.useDHCP = false;
   networking.defaultGateway = "192.168.10.1";
 
-  #  networking.firewall.allowedTCPPorts = [
-  #6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-  #2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-  #2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-  #];
-  #networking.firewall.allowedUDPPorts = [
-  #8472 # k3s, flannel: required if using multi-node for inter-node networking
-  #];
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
 }
